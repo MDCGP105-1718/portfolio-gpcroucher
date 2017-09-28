4f00775258e5d13795a213ff3ef15e8eb2eb764b
Ryan Manthorp <ryanmanthorp@hotmail.co.uk>
2017-09-28 11:53:21 +0100

    completed first four exercises

---
+++ ex4.py
@@ -0,0 +1,16 @@
+my_name = "George Croucher"
+my_age = 18
+my_height = 72 # inches
+my_weight = 11 * 14 # pounds
+my_eyes = "Green"
+my_hair = "Brown"
+is_heavy = my_weight > 3000
+
+print(f"Let's talk about {my_name}.")
+print(f"He is {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"It is {is_heavy} that he is overweight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
