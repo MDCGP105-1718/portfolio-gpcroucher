7e7a977adeb3ba579e5bcd3413ed2ed3b26c3ab9
Cafalump <calumbucknall@hotmail.co.uk>
2017-10-05 09:59:54 +0100

    Completed exercise 5

---
+++ ex5.py
@@ -0,0 +1,5 @@
+for n in range(100, 1, -1):
+    print(n, "bottles of beer on the wall,", n, "bottles of beer.\n Take one down, pass it around,", (n-1), "bottles of beer on the wall...")
+
+print("1 bottle of beer on the wall, 1 bottle of beer.\n Take one down, pass it around, no bottles of beer on the wall...")
+print("No more bottles of beer on the wall, no more bottles of beer.\n Go to the store and buy some more, 99 bottles of beer on the wall...")
