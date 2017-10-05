2477281c49bfe4660ef6c51ee6e06e8780836ed4
Cafalump <calumbucknall@hotmail.co.uk>
2017-10-05 10:49:43 +0100

    Fixed exercise 6

--- ex6.py
+++ ex6.py
@@ -9,4 +9,4 @@ weight = weight * 0.454 # convert to kilos
 height = height * 0.0254 # convert to metres
 
 print("How's it going? You are", name + ", you've been around for", age, "years.")
-print("Your BMI is", weight/height**2 + ", 30 and above is obese.")
+print("Your BMI is", str(round(weight/height**2, 2)) + ", 30 and above is obese.")
