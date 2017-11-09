b513d01a3cb8c8018dd802286af52b0879cf07cb
George Croucher <gpcroucher@gmail.com>
2017-11-09 09:45:42 +0000

    Added comments (still not working)

--- week5/ex13.py
+++ week5/ex13.py
@@ -1,3 +1,5 @@
+import os
+
 def script(): # main script loop
     lives = 9
     guessed_characters = []
@@ -5,22 +7,22 @@ def script(): # main script loop
 #    word = list(word) # converts word into a list of characters
     if " " in word: # if word contains a space
         return # exits the program
-    print(chr(27) + "[2J") # clears the terminal
-    clue = "_" * len(word)
+    os.system("cls") # clears the terminal
+    clue = "_" * len(word) # constructs a clue with underscores
     while lives > 0:
-        print(clue)
-        guess = str(input("Make a guess: ")).strip()
-        if len(guess) > 1:
+        print(clue) # show letters correctly guessed along with gaps
+        guess = str(input("Make a guess: ")).strip() # takes in a guess, discarding whitespace
+        if len(guess) != 1: # if guess is not a single character
             print("Single characters only please.")
-            continue
-        if guess in guessed_characters:
+            continue # back to prompt
+        if guess in guessed_characters: # if guess already guessed
             print("You already guessed this!")
-            continue
-        guessed_characters.append(guess)
-        if guess not in word:
-            lives -= 1
-            print()
-            continue
+            continue # back to prompt
+        guessed_characters.append(guess) # add to list of guessed characters
+        if guess not in word: # if guess is wrong
+            lives -= 1 # lose a life (starts at 9)
+            print(pic[lives]) # print the corresponding hanged man
+            continue # back to prompt
         for i in findOccurences(word, guess):
             s = list(clue)
             s[i] = guess
